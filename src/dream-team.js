d4dd6fe52b0022567eeea6f4d112651a6678a6c4
omen <omen@admin.ua>
2021-03-28 22:34:47 +0300

    fix : dreamteam

--- src/dream-team.js
+++ src/dream-team.js
@@ -5,10 +5,7 @@ module.exports = function createDreamTeam(members) {
   // remove line with error and write your code here
 
   if (!Array.isArray(members)) return false
-  members.forEach(el => {
-    if(typeof el != 'string') return false
-  });
-
+  
   word = members.reduce((acc,el)=> typeof el == 'string' ? el.trim().slice(0,1) + acc : acc,'').toUpperCase()
   word = word.split('').sort().join('')   //.filter((el, i) => (word.indexOf(el) == i))
 
