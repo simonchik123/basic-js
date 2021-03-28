573f192c1b385e592671cf71df09a9e86d2149b4
omen <omen@admin.ua>
2021-03-28 22:33:39 +0300

    add : 2 tasks

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,7 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
+module.exports = function countCats(matrix) {
+//  throw new CustomError('Not implemented');
   // remove line with error and write your code here
+  return matrix.reduce( (acc, el) => el.filter( cat => cat === '^^').length + acc, 0);
 };
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
+module.exports = function createDreamTeam(members) {
+  //throw new CustomError('Not implemented');
   // remove line with error and write your code here
+
+  if (!Array.isArray(members)) return false
+  members.forEach(el => {
+    if(typeof el != 'string') return false
+  });
+
+  word = members.reduce((acc,el)=> typeof el == 'string' ? el.trim().slice(0,1) + acc : acc,'').toUpperCase()
+  word = word.split('').sort().join('')   //.filter((el, i) => (word.indexOf(el) == i))
+
+
+  return (word != '') ? word : false
+
 };
