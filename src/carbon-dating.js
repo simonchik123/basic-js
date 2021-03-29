f4983e4f6beafab46428d7dec862202e4bd46ad2
simonchik123 <makc.baykov@gmail.com>
2021-03-29 15:04:26 +0300

    add : Hanoi & carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,20 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
+module.exports = function dateSample(sampleActivity) {
+  //throw new CustomError('Not implemented');
   // remove line with error and write your code here
+
+  if (typeof sampleActivity != 'string') return false
+  if (isNaN(parseFloat(sampleActivity.trim()))) return false
+
+   sampleActivity = parseFloat(sampleActivity.trim())
+
+  if (sampleActivity <= 0 || sampleActivity > MODERN_ACTIVITY) return false
+   
+    let rate_k = Math.log(2)/HALF_LIFE_PERIOD
+    t = Math.log(MODERN_ACTIVITY/sampleActivity)/rate_k
+
+    return Math.ceil(t)
+   
 };
--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,12 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new CustomError('Not implemented');
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+  //throw new CustomError('Not implemented');
   // remove line with error and write your code here
+
+  let turns = (disksNumber)=> 2**disksNumber-1
+  
+  let seconds = (turnsSpeed) => Math.floor(turns(disksNumber)/turnsSpeed*3600)
+  
+  return {turns:turns(disksNumber),seconds:seconds(turnsSpeed)}  
 };
