da1cf0b249d1cdf4a76310387a01ee3c61ddd7ca
simonchik123 <makc.baykov@gmail.com>
2021-03-29 22:26:38 +0300

    finish repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,12 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function repeater(str, options) {
-  let separator = '+'
-  let additionSeparator = '|'
 
+  str = '' + str
+  const addition = 'addition' in options ? '' + options.addition : '';
+  let separator = options.separator || '+'
+  let additionSeparator = options.additionSeparator || '|'
+  let repeatTimes = options.repeatTimes || 1    
+  let additionRepeatTimes = options.additionRepeatTimes || 1
+
+  let addStr = Array(additionRepeatTimes).fill(addition).join(additionSeparator)
+  str = Array(repeatTimes).fill(str+addStr).join(separator)
   
-
-
-  str = str.repeat(options.repeatTimes)
+  return str
 };
   
